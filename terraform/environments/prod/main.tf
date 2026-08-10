ee81299221849bd3686ebe697bb71d6590df1e30
Atiq Siddiqi <atiqtekpro@gmail.com>
2026-08-09 21:30:24 -0400

    Update module source paths in main.tf

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -1,31 +1,17 @@
-terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
 # Core Organizational Structure
 module "control_tower_ous" {
-  source = "../../modules/control_tower_ous"
+  source = "../modules/control_tower_ous"
 
-  root_id          = var.root_id
-  security_ou_name = "Security"
+  root_id           = var.root_id
+  security_ou_name  = "Security"
   workloads_ou_name = "Workloads"
 }
 
 # Automated Sandbox Account Provisioning
 module "sandbox_account" {
-  source = "../../modules/sandbox_account"
+  source = "../modules/sandbox_account"
 
-  parent_ou_id   = module.control_tower_ous.workloads_ou_id
-  account_name   = "sandbox-workload-01"
-  account_email  = var.sandbox_admin_email
+  parent_ou_id  = module.control_tower_ous.workloads_ou_id
+  account_name  = "sandbox-workload-01"
+  account_email = var.sandbox_admin_email
 }
